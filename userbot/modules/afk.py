8db26583df7b7b6152d966eea9e7fea8c16362dc
Anggit Wicaksono <77842877+metami123@users.noreply.github.com>
2021-02-10 18:46:16 +0700

    Update afk.py

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -17,29 +17,29 @@ from userbot.events import register
 
 # ========================= CONSTANTS ============================
 AFKSTR = [
-    "I'm busy right now. Please talk in a bag and when I come back you can just give me the bag!",
+    "Saya sedang mengerjakan hal lain, sabar ya, nanti saya jawab kalau sudah pegang HP lagi.",
     "I'm away right now. If you need anything, leave a message after the beep:\n`beeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeep`!",
-    "You missed me, next time aim better.",
+    "I'm away right now. Please wait, I'll get back to you once I'm available",
     "I'll be back in a few minutes and if I'm not...,\nwait longer.",
     "I'm not here right now, so I'm probably somewhere else.",
     "Roses are red,\nViolets are blue,\nLeave me a message,\nAnd I'll get back to you.",
     "Sometimes the best things in life are worth waiting for…\nI'll be right back.",
     "I'll be right back,\nbut if I'm not right back,\nI'll be back later.",
-    "If you haven't figured it out already,\nI'm not here.",
-    "Hello, welcome to my away message, how may I ignore you today?",
+    "If you don't know yet,\nI'm away from keyboard right now.",
+    "Hello, this is my away message, I'm not ignoring you.",
     "I'm away over 7 seas and 7 countries,\n7 waters and 7 continents,\n7 mountains and 7 hills,\n7 plains and 7 mounds,\n7 pools and 7 lakes,\n7 springs and 7 meadows,\n7 cities and 7 neighborhoods,\n7 blocks and 7 houses...\n\nWhere not even your messages can reach me!",
     "I'm away from the keyboard at the moment, but if you'll scream loud enough at your screen, I might just hear you.",
-    "I went that way\n---->",
-    "I went this way\n<----",
-    "Please leave a message and make me feel even more important than I already am.",
-    "I am not here so stop writing to me,\nor else you will find yourself with a screen full of your own messages.",
+    "Saya sedang di sana\n---->",
+    "Saya pergi ke sana\n<----",
+    "Please leave a message and I will respond as soon as I'm available.",
+    "Saya sedang jauh dari HP,\nsaya akan jawab nanti ya.",
     "If I were here,\nI'd tell you where I am.\n\nBut I'm not,\nso ask me when I return...",
-    "I am away!\nI don't know when I'll be back!\nHopefully a few minutes from now!",
-    "I'm not available right now so please leave your name, number, and address and I will stalk you later.",
+    "I am away!\nI don't know when I'll be back!\nHopefully a few minutes from now",
+    "I'm not available right now so please leave your name, number, and address and I will find you later.",
     "Sorry, I'm not here right now.\nFeel free to talk to my userbot as long as you like.\nI'll get back to you later.",
-    "I bet you were expecting an away message!",
-    "Life is so short, there are so many things to do...\nI'm away doing one of them..",
-    "I am not here right now...\nbut if I was...\n\nwouldn't that be awesome?",
+    "Maaf kalau pesan Anda tidak langsung saya jawab, saya lagi jauh dari HP!",
+    "Pesan Anda sangat penting..\nSaya sedang mencari jawaban..\nNanti saya jawab kalau sudah menemukan jawabannya",
+    "Saya sedang jauh dari HP\natau saya sedang mencari HP saya untuk menjawab pesan Anda\nsabar ya",
 ]
 
 global USER_AFK  # pylint:disable=E0602
@@ -177,7 +177,7 @@ async def mention_afk(mention):
             if mention.sender_id not in USERS:
                 if AFKREASON:
                     await mention.reply(
-                        f"I'm AFK since {afk_since}.\
+                        f"I'm Away From Keyboard since {afk_since}.\
                         \nReason: `{AFKREASON}`"
                     )
                 else:
@@ -188,7 +188,7 @@ async def mention_afk(mention):
                 if USERS[mention.sender_id] % randint(2, 4) == 0:
                     if AFKREASON:
                         await mention.reply(
-                            f"I'm still AFK since {afk_since}.\
+                            f"I'm still Away From Keyboard since {afk_since}.\
                             \nReason: `{AFKREASON}`"
                         )
                     else:
@@ -261,7 +261,7 @@ async def afk_on_pm(sender):
             if sender.sender_id not in USERS:
                 if AFKREASON:
                     await sender.reply(
-                        f"I'm AFK since {afk_since}.\
+                        f"I'm Away From Keyboard since {afk_since}.\
                         \nReason: `{AFKREASON}`"
                     )
                 else:
@@ -272,7 +272,7 @@ async def afk_on_pm(sender):
                 if USERS[sender.sender_id] % randint(2, 4) == 0:
                     if AFKREASON:
                         await sender.reply(
-                            f"I'm still AFK since {afk_since}.\
+                            f"I'm still Away From Keyboard since {afk_since}.\
                             \nReason: `{AFKREASON}`"
                         )
                     else:
